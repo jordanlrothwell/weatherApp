f5a767960896eb12e7adfb16501e2f94b250842b
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-01-12 14:06:29 +1000

    Added functions to convert temp + extract data from API results.

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,29 @@
 let cityName = "London";
 let apiKey = "e76b72821d51dc3558071ffa27cf4d8d";
-let weatherURL = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
-console.log(weatherURL)
+console.log(weatherURL);
 
+let weatherInfo = {};
 
-const getWeather = function () {
-  fetch(weatherURL).then(function (response) {
-    if (!response.ok) {
-      throw response.json();
-    }
-
-    return response.json();
-  })
-    .then(function(result) {
-        console.log(result)
+let getWeather = function () {
+  fetch(weatherURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (result) {
+      console.log(result)
     });
 };
+
+// FUNCTION: converts Kelvin to Celcius (rounding to 1 d.p.)
+let k2c = function (k) {
+  return (k - 273.15).toFixed(1);
+};
+
+let extractData = function (resultObj) {
+  console.log(resultObj.weather[0].main);
+  console.log(k2c(resultObj.main.temp));
+}
 
 getWeather();
