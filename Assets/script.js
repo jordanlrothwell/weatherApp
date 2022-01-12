be761f56712afcc36f76c53ae72b840fe2bb66cd
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-01-12 14:28:45 +1000

    Changed from 'forecast' to 'one call' API call to capture UVI.

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,8 @@
 let cityName = "London";
+let latitude = 51.5085;
+let longitude = -0.1257;
 let apiKey = "e76b72821d51dc3558071ffa27cf4d8d";
-let weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+let weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&appid=${apiKey}`;
 
 console.log(weatherURL);
 
@@ -12,7 +14,7 @@ let getWeather = function () {
       return response.json();
     })
     .then(function (result) {
-     extractData(result);
+      extractData(result);
     });
 };
 
@@ -21,12 +23,20 @@ let k2c = function (k) {
   return (k - 273.15).toFixed(1);
 };
 
+let unixToDate = function (unix) {
+  return new Date(unix * 1000);
+};
+
 let extractData = function (resultObj) {
-  for (i = 0; i < resultObj.list.length; i++) {
+  for (i = 0; i < resultObj.daily.length; i++) {
     console.log(
-      `Weather for ${resultObj.list[i].dt_txt}: ${resultObj.list[i].weather[0].main}`
+      `Weather for ${unixToDate(resultObj.daily[i].dt)}: ${
+        resultObj.daily[i].weather[0].main
+      }`
     );
   }
 };
 
 getWeather();
+
+console.log(unixToDate(1641988800));
